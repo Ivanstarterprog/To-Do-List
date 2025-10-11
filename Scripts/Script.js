85981a5f69268d1654f69568ebdfb5c8ce07e0ce
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2025-10-12 01:48:56 +0700

    Создание класса Task

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,4 +1,21 @@
+class Task{
+    constructor(title, body){
+        this.title = title;
+        this.body = body;
+    }
+
+    setDeadLineStart(data = "Неуказано"){
+        this.deadLineStart = data;
+    }
+
+    setDeadLineEnd(data = "Неуказано"){
+        this.deadLineEnd = data;
+    }
+}
+
+var tasks = new Array();
+
 window.addEventListener("load", ()=>{
-    let tasks = new Array();
     
+
 })
