8aefd2a49d052858269a3a9605b7bbfdee8dc220
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2025-10-12 01:59:55 +0700

    Создание функции, добавляющей задачу в список задач

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -15,7 +15,26 @@ class Task{
 
 var tasks = new Array();
 
+var addTask = (title, body)=>{
+    tasks.push(
+        new Task(title, body)
+    )
+}
+
 window.addEventListener("load", ()=>{
-    
+    let newTaskTitle = document.getElementById("newTaskTitle")
+    let newTaskBody = document.getElementById("newTaskBody")
+    let addNewTask = document.getElementById("addNewTaskButton")
+    addNewTask.addEventListener("click", ()=>{
+        addTask(
+            newTaskTitle.value,
+            newTaskBody.value
+        )
+        newTaskTitle.value = ""
+        newTaskBody.value = ""
+        console.log(tasks);
+    })
+
 
 })
+
