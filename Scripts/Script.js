bd1cabf56aa66336e3f41c8e2b2244416afb6c6b
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2025-10-12 03:21:07 +0700

    Создание функции, удаляющей задачу из графического интерфейса

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,5 +1,7 @@
 class Task{
+    static nextId = 0;
     constructor(title, body = "У задачи нет описания"){
+        this.id = Task.nextId++; 
         this.title = title;
         this.body = body;
         this.deadLineStart = "Не указано"
@@ -36,19 +38,11 @@ var addTaskToList = (task) => {
     )
 }
 
-var reloadTasksList = () => {
-    let taskList = document.getElementById("tasks");
-    taskList.textContent = ""
-    for (task in tasks){
-        addTaskToInterface(task);
-    }
-}
-
 var addTaskToInterface = (task) => {
     let taskList = document.getElementById("tasks");
     const newTaskDiv = document.createElement("div");
-    newTaskDiv.classList.add("task-card-container")
     newTaskDiv.innerHTML = `
+        <div class="task-card-container" id="task#${task.id}">
           <div class="task__card">
             <div class="task__card__data">
               <h3 class="task__card__title">
@@ -59,7 +53,7 @@ var addTaskToInterface = (task) => {
               </p>
               <h4 class="task__card__deadline">${task.deadLine()}</h4>
             </div>
-            <button class="delete__task__button"> 
+            <button class="delete__task__button" task-id="${task.id}"> 
                 <img src="../Assets/Img/Union.svg" alt="Удалить" />
             </button>
           </div>
@@ -74,10 +68,36 @@ var addTaskToInterface = (task) => {
                 <img src="../Assets/Img/Edit.svg" alt="Редактировать" />
             </button>
           </div>
+        </div>
     `;
     
     taskList.appendChild(newTaskDiv.firstElementChild);
 }
+
+var deleteTask = (taskID) =>{
+    deleteTaskFromList(taskID)
+    deleteTaskFromInterface(taskID)
+}
+
+var deleteTaskFromList = (taskID) => {
+    tasks = tasks.filter(task => task.id != taskID)
+}
+
+var deleteTaskFromInterface = (taskID) => {
+    let task = document.getElementById(`task#${taskID}`)
+    if (task) {
+        task.remove()
+    }
+}
+
+var reloadTasksList = () => {
+    let taskList = document.getElementById("tasks");
+    taskList.textContent = ""
+    for (task in tasks){
+        addTaskToInterface(task);
+    }
+}
+
 
 window.addEventListener("load", ()=>{
     let newTaskTitle = document.getElementById("newTaskTitle")
@@ -92,7 +112,12 @@ window.addEventListener("load", ()=>{
         newTaskBody.value = ""
         console.log(tasks);
     })
-
+    document.body.addEventListener('click', function(event) {
+        if (event.target.classList.contains('delete__task__button')) {
+            id = event.target.getAttribute('task-id')
+            deleteTask(id)  
+        }
+    });
 
 })
 
