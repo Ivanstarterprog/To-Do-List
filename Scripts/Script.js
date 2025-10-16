a239dad2eff3eba543cdb6c3ef21edd158e419d3
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2025-10-17 00:04:36 +0700

    Добавление всплывающего окна изменения задачи

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,225 +1,292 @@
-class Task{
-    static nextId = 0;
-    constructor(title, body = "У задачи нет описания"){
-        this.id = Task.nextId++; 
-        this.title = title;
-        this.body = body;
-        this.fullDescription = ""
-        this.deadLineStart = new Date().toLocaleDateString('ru-RU');
-        this.deadLineEnd = "Не указано"
-    }
+class Task {
+  static nextId = 0;
+  constructor(title, body = "У задачи нет описания") {
+    this.id = Task.nextId++;
+    this.title = title;
+    this.body = body;
+    this.description = "";
+    this.deadLineStart = new Date().toLocaleDateString("ru-RU");
+    this.deadLineEnd = "Не указано";
+  }
 
-    deadLine(){
-        return `${this.deadLineStart} - ${this.deadLineEnd}`
-    }
+  deadLine() {
+    return `${this.deadLineStart} - ${this.deadLineEnd}`;
+  }
 
-    setDeadLineStart(data){
-        this.deadLineStart = data;
-    }
+  setTaskTitle(newTitle){
+    this.title = newTitle
+  }
 
-    setDeadLineEnd(data){
-        this.deadLineEnd = data;
-    }
+  setTaskBody(newBody){
+    this.body = newBody
+  }
+
+  setTaskDescription(newFullDescription){
+    this.description = newFullDescription
+  }
+
+  setTaskNewData(taskData){
+    this.setTaskTitle(taskData.title)
+    this.setTaskBody(taskData.body)
+    this.setTaskDescription(taskData.description)
+  }
+
+  setDeadLineEnd(data) {
+    this.deadLineEnd = data;
+  }
 }
 
 var tasks = new Array();
 
-var addTask = (tittle, body) =>{
-    if (tittle == "") {
-        return
-    }
-    let newTask = new Task(tittle, body)
-    addTaskToList(newTask)
-    addTaskToInterface(newTask)
-    checkNumberOfTasks()
-}
+var addTask = (tittle, body) => {
+  if (tittle == "") {
+    return;
+  }
+  let newTask = new Task(tittle, body);
+  addTaskToList(newTask);
+  addTaskToInterface(newTask);
+  checkNumberOfTasks();
+};
 
 var addTaskToList = (task) => {
-    tasks.push(
-        task
-    )
-}
+  tasks.push(task);
+};
 
 var addTaskToInterface = (task) => {
-    let taskList = document.getElementById("tasks");
-    const newTaskDiv = document.createElement("div");
-    newTaskDiv.innerHTML = `
-        <div class="task-card-container" id="task#${task.id}">
-          <div class="task__card" task-id="${task.id}">
-            <div class="task__card__data">
-              <h3 class="task__card__title">
-                ${task.title}
-              </h3>
-              <p class="task__card__description">
-                ${task.body}
-              </p>
-              <h4 class="task__card__deadline">${task.deadLine()}</h4>
-            </div>
-            <button class="delete__task__button" task-id="${task.id}"> 
-                <img src="../Assets/Img/Union.svg" alt="Удалить" />
-            </button>
-          </div>
-          <div class="task__card__buttons-container" task-id="${task.id}" id="task-buttons#${task.id}">
-            <button class="task__card__buttons task__card__share-button">
-                <img src="../Assets/Img/Share.svg" alt="Поделиться" />
-            </button>
-            <button class="task__card__buttons  task__card__share-butto">
-                <img src="../Assets/Img/i.svg" alt="Информация" />
-            </button>
-            <button class="task__card__buttons cardEditButton">
-                <img src="../Assets/Img/Edit.svg" alt="Редактировать" />
-            </button>
-          </div>
-        </div>
+  let taskList = document.getElementById("tasks");
+  const newTaskDiv = document.createElement("div");
+  console.log(task)
+  newTaskDiv.innerHTML = `
+    <div class="task-card-container" id="task#${task.id}">
+    <div class="task__card" task-id="${task.id}">
+    <div class="task__card__data">
+    <h3 class="task__card__title">
+    ${task.title}
+    </h3>
+    <p class="task__card__description">
+    ${task.body}
+    </p>
+    <h4 class="task__card__deadline">${task.deadLine()}</h4>
+    </div>
+    <button class="delete__task__button" task-id="${task.id}"> 
+    <img src="../Assets/Img/Union.svg" alt="Удалить" />
+    </button>
+    </div>
+    <div class="task__card__buttons-container" task-id="${
+      task.id
+    }" id="task-buttons#${task.id}">
+    <button class="task__card__buttons task__card__share-button">
+    <img src="../Assets/Img/Share.svg" alt="Поделиться" />
+    </button>
+    <button class="task__card__buttons  task__card__info-button">
+    <img src="../Assets/Img/i.svg" alt="Информация" />
+    </button>
+    <button class="task__card__buttons task__card__edit-button" task-id="${task.id}">
+    <img src="../Assets/Img/Edit.svg" alt="Редактировать" />
+    </button>
+    </div>
+    </div>
     `;
-    
-    taskList.appendChild(newTaskDiv.firstElementChild);
-}
 
-var deleteTask = async (taskID) =>{
-    showModalWindow('confirmModal')
-    const confirmDeletion = await confirmDeletionOfTask()
-    hideModalWindow('confirmModal')
-    if (!confirmDeletion){
-        return
-    }
-    deleteTaskFromList(taskID)
-    deleteTaskFromInterface(taskID)
-    checkNumberOfTasks()
-}
+  taskList.appendChild(newTaskDiv.firstElementChild);
+};
 
-var deleteTaskFromList = (taskID) => {
-    tasks = tasks.filter(task => task.id != taskID)
-}
-
-var deleteTaskFromInterface = (taskID) => {
-    let task = document.getElementById(`task#${taskID}`)
-    if (task) {
-        task.remove()
-    }
-}
-
-var checkNumberOfTasks = () => {
-    if (tasks.length > 0) {
-        hideThereIsNoTasksCard()
-        return
-    }
-
-    showThereIsNoTasksCard()
-}
-
-var hideThereIsNoTasksCard = () => {
-    let thereIsNoTasksCard = document.getElementById("noTasksCard")
-    thereIsNoTasksCard.style.display = "none"
-}
-
-var showThereIsNoTasksCard = () => {
-    let thereIsNoTasksCard = document.getElementById("noTasksCard")
-    thereIsNoTasksCard.style.display = "flex"
-}
-
-var reloadTasksList = () => {
-    let taskList = document.getElementById("tasks");
-    taskList.textContent = ""
-    for (task in tasks){
-        addTaskToInterface(task);
-    }
-}
-
-var toggleTaskButtons = (taskID) => {
-    let taskButtons = document.getElementById(`task-buttons#${taskID}`)
-    if (!taskButtons){
-        return
-    }
-    hideAllNotClickedTaskButtons(taskID)
-    if (taskButtons.style.display == "flex"){
-        taskButtons.style.display = "none"
-        return
-    }
-    taskButtons.style.display = "flex"
-}
-
-var hideAllNotClickedTaskButtons = (taskID) => {
-    document.querySelectorAll('.task__card__buttons-container').forEach(container => {
-        containerID = container.getAttribute('task-id')
-        if (containerID != taskID){
-            container.style.display = 'none';
-        }
-    });
-}
-
-var hideAllTaskButtons = () => {
-    document.querySelectorAll('.task__card__buttons-container').forEach(container => {
-        container.style.display = 'none';
-    });
-}
-
-var showShareMenu = () => {
-    showModalWindow("shareModal")
-}
-
-var hideShareMenu = () => {
-    hideModalWindow("shareModal")
-}
-
+var deleteTask = async (taskID) => {
+  showModalWindow("confirmModal");
+  const confirmDeletion = await confirmModal("confirmDelete", "cancelDelete");
+  hideModalWindow("confirmModal");
+  if (!confirmDeletion) {
+    return;
+  }
+  deleteTaskFromList(taskID);
+  deleteTaskFromInterface(taskID);
+  checkNumberOfTasks();
+};
 
 var showModalWindow = (id) => {
-    const modal = document.getElementById(id);
-    modal.style.display = 'flex';
-    modal.addEventListener("click", () => {
-        hideModalWindow(id)
-    })
-}
+  const modal = document.getElementById(id);
+  modal.style.display = "flex";
+  const modalClickHandler = (event) => {
+    if (event.target === modal) {
+      hideModalWindow(id);
+      modal.removeEventListener("click", modalClickHandler);
+    }
+  };
+  
+  modal.addEventListener("click", modalClickHandler);
+};
 
 var hideModalWindow = (id) => {
-    const modal = document.getElementById(id);
-    modal.style.display = 'none';
+  const modal = document.getElementById(id);
+  modal.style.display = "none";
+};
+
+
+var confirmModal = (confirmButtonID, cancelButtonID) => {
+  return new Promise((resolve) => { 
+    const confirmButton = document.getElementById(confirmButtonID)
+    const cancelButton = document.getElementById(cancelButtonID);
+
+    confirmButton.onclick = () => {
+      resolve(true);
+    };
+
+    cancelButton.onclick = () => {
+      resolve(false);
+    };
+  });
+};
+
+var deleteTaskFromList = (taskID) => {
+  tasks = tasks.filter((task) => task.id != taskID);
+};
+
+var deleteTaskFromInterface = (taskID) => {
+  let task = document.getElementById(`task#${taskID}`);
+  if (task) {
+    task.remove();
+  }
+};
+
+var showShareMenu = () => {
+  showModalWindow("shareModal");
+};
+
+var hideShareMenu = () => {
+  hideModalWindow("shareModal");
+};
+
+var showEditMenu = async (taskID) => {
+    showModalWindow("editModal")
+    updateEditMenuInformation(taskID)
+    const confirmChangeOfTask = await confirmModal("confirmEdit", "cancelEdit")
+    hideModalWindow("editModal")
+    if(!confirmChangeOfTask){
+      return
+    }
+    editTaskInformation(taskID)
+    reloadTasksList()
+};
+
+var updateEditMenuInformation = (taskID) => {
+  let titleInput = document.getElementById(`titleEdit`);
+  let bodyInput = document.getElementById(`bodyEdit`);
+  let descriptionInput = document.getElementById(`descriptionEdit`);
+  let task = tasks.find(task => task.id == taskID)
+  titleInput.value = task.title
+  bodyInput.value = task.body
+  descriptionInput.value = task.description
 }
 
-var confirmDeletionOfTask = () => {
-    return new Promise((resolve) => {
-        const confirmBtn = document.getElementById('confirmDelete');
-        const cancelBtn = document.getElementById('cancelDelete');
-
-        confirmBtn.onclick = () => {
-            resolve(true);
-        };
-
-        cancelBtn.onclick = () => {
-            resolve(false); 
-        };
-    });
-}
-
-window.addEventListener("load", ()=>{
-    let newTaskTitle = document.getElementById("newTaskTitle")
-    let newTaskBody = document.getElementById("newTaskBody")
-    let addNewTask = document.getElementById("addNewTaskButton")
-    addNewTask.addEventListener("click", ()=>{
-        addTask(
-            newTaskTitle.value, 
-            newTaskBody.value
-        )
-        newTaskTitle.value = ""
-        newTaskBody.value = ""
+var editTaskInformation = (taskID) =>{
+  let newTitle = document.getElementById(`titleEdit`);
+  if (newTitle.value.trim() == ""){
+    return
+  }
+  let newBody = document.getElementById(`bodyEdit`);
+  let newDescription = document.getElementById(`descriptionEdit`);
+  let task = tasks.find(task => task.id == taskID)
+  if (task){
+    task.setTaskNewData({
+      title: newTitle.value,
+      body: newBody.value,
+      description: newDescription.value
     })
-    document.body.addEventListener('click', function(event) {
-        if (event.target.classList.contains('delete__task__button')) {
-            let id = event.target.getAttribute('task-id')
-            deleteTask(id)  
-        }
-        if (event.target.classList.contains('task__card__share-button')){
-            console.log("Try to open share")
-            showShareMenu()
-        }
-        if (event.target.classList.contains('task__card') ) {
-            id = event.target.getAttribute('task-id') 
-            toggleTaskButtons(id)
-        }
-        else if (!event.target.classList.contains("task__card__buttons")){
-            hideAllTaskButtons()
-        }
+  }
+}
+
+
+var checkNumberOfTasks = () => {
+  if (tasks.length > 0) {
+    hideThereIsNoTasksCard();
+    return;
+  }
+
+  showThereIsNoTasksCard();
+};
+
+var hideThereIsNoTasksCard = () => {
+  let thereIsNoTasksCard = document.getElementById("noTasksCard");
+  thereIsNoTasksCard.style.display = "none";
+};
+
+var showThereIsNoTasksCard = () => {
+  let thereIsNoTasksCard = document.getElementById("noTasksCard");
+  thereIsNoTasksCard.style.display = "flex";
+};
+
+var reloadTasksList = () => {
+  let taskList = document.getElementById("tasks");
+  taskList.textContent = "";
+  console.log(tasks)
+  for (let task of tasks) {
+    console.log(task)
+    addTaskToInterface(task);
+  }
+};
+
+var toggleTaskButtons = (taskID) => {
+  let taskButtons = document.getElementById(`task-buttons#${taskID}`);
+  if (!taskButtons) {
+    return;
+  }
+  hideAllNotClickedTaskButtons(taskID);
+  if (taskButtons.style.display == "flex") {
+    taskButtons.style.display = "none";
+    return;
+  }
+  taskButtons.style.display = "flex";
+};
+
+var hideAllNotClickedTaskButtons = (taskID) => {
+  document
+    .querySelectorAll(".task__card__buttons-container")
+    .forEach((container) => {
+      containerID = container.getAttribute("task-id");
+      if (containerID != taskID) {
+        container.style.display = "none";
+      }
     });
+};
 
-})
+var hideAllTaskButtons = () => {
+  document
+    .querySelectorAll(".task__card__buttons-container")
+    .forEach((container) => {
+      container.style.display = "none";
+    });
+};
 
+window.addEventListener("load", () => {
+  let newTaskTitle = document.getElementById("newTaskTitle");
+  let newTaskBody = document.getElementById("newTaskBody");
+  let addNewTask = document.getElementById("addNewTaskButton");
+  addNewTask.addEventListener("click", () => {
+    addTask(newTaskTitle.value, newTaskBody.value);
+    newTaskTitle.value = "";
+    newTaskBody.value = "";
+  });
+  document.body.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete__task__button")) {
+      let id = event.target.getAttribute("task-id");
+      deleteTask(id);
+    }
+    if (event.target.classList.contains("task__card__info-button")) {
+      let id = event.target.getAttribute("task-id");
+      showEditMenu(id);
+    }
+    if (event.target.classList.contains("task__card__edit-button")) {
+      let id = event.target.getAttribute("task-id");
+      showEditMenu(id);
+    }
+    if (event.target.classList.contains("task__card__share-button")) {
+      showShareMenu();
+    }
+    if (event.target.classList.contains("task__card")) {
+      id = event.target.getAttribute("task-id");
+      toggleTaskButtons(id);
+    } else if (!event.target.classList.contains("task__card__buttons")) {
+      hideAllTaskButtons();
+    }
+  });
+});
